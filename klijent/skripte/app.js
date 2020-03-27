e57c5562bc35a606a773835eaaa14a26a7bc4fb7
acadragon <62178571+acadragon@users.noreply.github.com>
2020-03-27 11:36:46 +0100

    Update app.js

--- klijent/skripte/app.js
+++ klijent/skripte/app.js
@@ -185,24 +185,17 @@ function ispraziKorpu() {
     korpa.setItem("porudzbine", JSON.stringify([]));
   }
 }
-function validateEmail(email) {
-  var re = /^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
+$( "#email" ).blur(function() {
+
+let re = /^(([^<>()[].,;:\s@"]+(.[^<>()[].,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+
+let isValid = re.test($( "#email" ).val());
+
+If (isValid) {
+$( "#email" ).css("color", "green");
+} else {
+$( "#email" ).css("color", "red");
 }
 
-function validate() {
-  var $result = $("#result");
-  var email = $("#email").val();
-  $result.text("");
-
-  if (validateEmail(email)) {
-    $result.text(email + " is valid :)");
-    $result.css("color", "green");
-  } else {
-    $result.text(email + " is not valid :(");
-    $result.css("color", "red");
-  }
-  return false;
-}
-
+});
 ucitaj("../server/baza/bombone.json");
