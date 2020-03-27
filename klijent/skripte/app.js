0a0b06971a5b3c5196baa2c3aaeff4c192583511
acadragon <62178571+acadragon@users.noreply.github.com>
2020-03-27 11:50:30 +0100

    Update app.js

--- klijent/skripte/app.js
+++ klijent/skripte/app.js
@@ -185,17 +185,25 @@ function ispraziKorpu() {
     korpa.setItem("porudzbine", JSON.stringify([]));
   }
 }
-$( "#email" ).blur(function() {
-
-let re = /^(([^<>()[].,;:\s@"]+(.[^<>()[].,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
-
-let isValid = re.test($( "#email" ).val());
-
-if (isValid) {
-$( "#email" ).css("color", "green");
-} else {
-$( "#email" ).css("color", "red");
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 }
 
-});
+function validate() {
+  var $result = $("#result");
+  var email = $("#email").val();
+  $result.text("");
+
+  if (validateEmail(email)) {
+    $result.text(email + " je validan");
+    $result.css("color", "green");
+  } else {
+    $result.text(email + " nije validan");
+    $result.css("color", "red");
+  }
+  return false;
+}
+
+$("#order-button").on("click", validate);
 ucitaj("../server/baza/bombone.json");
